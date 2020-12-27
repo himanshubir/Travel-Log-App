2e05933cafe095ea49f7481ae9e7ce71122a3d62
himanshubir <68207231+himanshubir@users.noreply.github.com>
2020-12-27 15:05:36 -0500

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -21,28 +21,15 @@ app.use(morgan('common')); // Sample log in terminal ::1 - - [20/Nov/2020:03:52:
 app.use(helmet({
     contentSecurityPolicy: false,
 }));
-console.log(process.env.CORS_ORIGIN);
+
 app.use(cors({
-    origin: process.env.CORS_ORIGIN, // Only requests coming from this location can reach our backend
+    origin: 'https://travel-log-app-himanshu.herokuapp.com' // Only requests coming from this location can reach our backend
 }));
 app.use(express.json()); // Body Parsing Middleware
 const port = process.env.PORT || 5000;
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, '../../client/build')));
 }
-app.use((req, res, next) => {
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Pass to next layer of middleware
-    next();
-  });
 app.use('/', logs);
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
