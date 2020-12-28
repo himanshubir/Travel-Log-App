3591d1aca349ce2f1f71bc0508ccc364909f078f
himanshubir <68207231+himanshubir@users.noreply.github.com>
2020-12-27 22:43:04 -0500

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -22,7 +22,7 @@ app.use(helmet({
     contentSecurityPolicy: false,
 }));
 app.use(cors({
-    origin:process.env.CORS_ORIGIN
+    origin:'https://travel-log-app-himanshu.herokuapp.com/'
 }));
 app.use(express.json()); // Body Parsing Middleware
 const port = process.env.PORT || 5000;
